42c27203a18fec166da3513b38ad91289928c5ce
ghop1073 <gagehopkins1@gmail.com>
2016-03-30 13:22:39 -0600

    changed imports

--- src/chat/controller/IOController.java
+++ src/chat/controller/IOController.java
@@ -1,6 +1,32 @@
 package chat.controller;
 
+import java.io.*;
+import java.util.*;
+import javax.swing.*;
+
 public class IOController
 {
 
+	public static String saveFile(String textToSave)
+	{
+		String fileName = "Saved Chat File - ";
+		fileName += Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+		fileName += ":" + Calendar.getInstance().get(Calendar.MINUTE);
+		fileName += ".txt";
+		FileWriter chatWriter;
+		try
+		{
+			chatWriter = new FileWriter(fileName);
+			chatWriter.write("This is the " + fileName + "\n");
+			chatWriter.write(textToSave);
+			chatWriter.close();
+			JOptionPane.showMessageDialog(null, "File saved as: " + fileName);
+		}
+		catch(IOException ioError)
+		{
+			JOptionPane.showMessageDialog(null, ioError.getMessage());
+		}
+		
+		return fileName;
+	}
 }
